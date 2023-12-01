ec57d3cea021f8d8d075e00f2079d4fd9be1ba61
Til Goepfert <tilgoepfert@gmail.com>
2023-12-01 16:25:09 +0100

    rewrote to properly handle puzzle 2

--- day01/day01.py
+++ day01/day01.py
@@ -1,50 +1,70 @@
 import re
 from os import path
 
-sum = 0
-
-inputPath = path.join(path.dirname(__file__), 'input.txt')
 
 def replace_digits(reg_match):
-  digit = reg_match.group()
-  print(f'  {digit}')
-  match digit:
-    case 'one':
-      return '1'
-    case 'two':
-      return '2'
-    case 'three':
-      return '3'
-    case 'four':
-      return '4'
-    case 'five':
-      return '5'
-    case 'six':
-      return '6'
-    case 'seven':
-      return '7'
-    case 'eight':
-      return '8'
-    case 'nine':
-      return '9'
+    digit = reg_match.group()
+    match digit:
+        case "one":
+            return 1
+        case "two":
+            return 2
+        case "three":
+            return 3
+        case "four":
+            return 4
+        case "five":
+            return 5
+        case "six":
+            return 6
+        case "seven":
+            return 7
+        case "eight":
+            return 8
+        case "nine":
+            return 9
 
-file = open(inputPath, 'r')
+
+def extract_digit_at(line, index):
+    print(f"      index: {index}; char: {line[index]}")
+    if line[index].isdigit():
+        return int(line[index])
+    else:
+        match = re.match(
+            r"^(one|two|three|four|five|six|seven|eight|nine)", line[index : index + 5]
+        )
+        if match:
+            return replace_digits(match)
+
+
+def find_first_digit(line):
+    for i in range(len(line)):
+        digit = extract_digit_at(line, i)
+        if digit:
+            return digit
+
+
+def find_last_digit(line):
+    for i in range(len(line))[::-1]:
+        digit = extract_digit_at(line, i)
+        if digit:
+            return digit
+
+
+sum = 0
+
+inputPath = path.join(path.dirname(__file__), "input.txt")
+file = open(inputPath, "r")
 lines = file.read().splitlines()
 
-for line in lines[:10]:
-  print(line)
-  line = re.sub(r'one|two|three|four|five|six|seven|eight|nine', replace_digits, line)
-  print(f'  {line}')
-  # strip characters
-  digits = re.sub(r'[a-z]+', '', line)
-  # print(f'  {digits}')
-  # digits = re.findall(r'\d', line)
+for line in lines:
+    print(line)
+    first = find_first_digit(line)
+    last = find_last_digit(line)
 
-  first = int(digits[0])
-  last = int(digits[-1])
+    print(f"  {first}")
+    print(f"  {last}")
 
-  # print(f'    first: {first}')
-  # print(f'    last: {last}')
-  sum += 10 * first + last
+    sum += 10 * first + last
 
 print(sum)
