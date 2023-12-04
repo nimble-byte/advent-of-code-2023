7274c3b3cd14ea1d23f4d6200486537c7fe78d3a
Til Goepfert <tilgoepfert@gmail.com>
2023-12-04 23:35:26 +0100

    disentagled file for day 2

---
+++ day02/part1.py
@@ -0,0 +1,41 @@
+from os import path
+import re
+
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+
+def get_max_color_count(hand, color):
+    pattern = f"(\\d+) {color}"
+    counts = re.findall(pattern, hand)
+    if len(counts) == 0:
+        return 0
+    else:
+        return max([int(count) for count in counts])
+
+
+def extract_game_info(line):
+    game = {}
+    gameid, hands = line.split(":")
+
+    game["game"] = int(gameid.split(" ")[1])
+    game["red"] = get_max_color_count(hands, "r")
+    game["green"] = get_max_color_count(hands, "g")
+    game["blue"] = get_max_color_count(hands, "b")
+
+    return game
+
+result = 0
+
+lines = []
+with open(path.join(path.dirname(__file__), "input.txt")) as f:
+    lines = f.read().splitlines()
+
+for line in lines:
+    game = extract_game_info(line)
+
+    if game["red"] <= MAX_RED and game["green"] <= MAX_GREEN and game["blue"] <= MAX_BLUE:
+        result += game["game"]
+
+print(f"The result for part 1 is {result}")
--- day02/part2.py
+++ day02/part2.py
@@ -16,34 +16,26 @@ def get_max_color_count(hand, color):
 
 
 def extract_game_info(line):
-    game = []
+    game = {}
     gameid, hands = line.split(":")
 
-    game.append(int(gameid.split(" ")[1]))
-    game.append(get_max_color_count(hands, "r"))
-    game.append(get_max_color_count(hands, "g"))
-    game.append(get_max_color_count(hands, "b"))
+    game["game"] = int(gameid.split(" ")[1])
+    game["red"] = get_max_color_count(hands, "r")
+    game["green"] = get_max_color_count(hands, "g")
+    game["blue"] = get_max_color_count(hands, "b")
 
     return game
 
+result = 0
 
-# Read input
 lines = []
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
     lines = f.read().splitlines()
 
-sum_part_1 = 0
-sum_part_2 = 0
-
 for line in lines:
     game = extract_game_info(line)
 
-    # Part 1
-    if game[1] <= MAX_RED and game[2] <= MAX_GREEN and game[3] <= MAX_BLUE:
-        sum_part_1 += game[0]
 
-    # Part 2
-    sum_part_2 += game[1] * game[2] * game[3]
+    result += game["red"] * game["green"] * game["blue"]
 
-print(f"The sum for part 1 is {sum_part_1}")
-print(f"The sum for part 2 is {sum_part_2}")
+print(f"The result for part 1 is {result}")
