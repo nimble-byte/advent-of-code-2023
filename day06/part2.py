a5494eb051eb9deb85f58ae2313ab2c2ad3eb36b
Til Goepfert <tilgoepfert@gmail.com>
2023-12-06 15:15:55 +0100

    solving day06 puzzle 2 in a smart-ish way

--- day06/part2.py
+++ day06/part2.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import ceil, floor, sqrt
 from os import path
 
 
@@ -22,6 +22,22 @@ class Race:
                 solutions.append(distance)
         return solutions
 
+    def solutions_smart(self):
+        # calculate the number of solutions directly by calculating minimum and maximum holding time
+        # distance travelled = d
+        # time holding = h
+        # overall time = t
+        # d = (t - h) * h => 0 = h^2 - th + d
+        # use quadratic formula to solve for bounds
+        # lb = (t - sqrt(t^2 - 4d)) / 2
+        # ub = (t + sqrt(t^2 - 4d)) / 2
+        print(f"Time: {self.time}, Distance: {self.distance}")
+        lb = ceil((self.time - sqrt(self.time ** 2 - 4 * self.distance)) / 2)
+        ub = floor((self.time + sqrt(self.time ** 2 - 4 * self.distance)) / 2)
+
+        return ub - lb + 1
+
+
 
 lines = []
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
@@ -31,6 +47,9 @@ time = int(lines[0].replace(" ", "").split(":")[1])
 distance = int(lines[1].replace(" ", "").split(":")[1])
 
 race = Race(time, distance)
-solutions = race.solutions()
+# solutions = race.solutions()
 
-print(f"Number of solutions: {len(solutions)}")
+# print(f"Number of solutions: {len(solutions)}")
+solutions = race.solutions_smart()
+print(f"Number of solutions: {solutions}")
+
